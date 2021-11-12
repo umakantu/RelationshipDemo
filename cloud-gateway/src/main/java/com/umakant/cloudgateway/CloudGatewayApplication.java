9aeb08c51cf7d3ff23de510e29841b1c1923527c
Umakant <Umakant@DESKTOP-O49BCT7>
2021-11-12 19:59:11 +0530

    1

--- cloud-gateway/src/main/java/com/umakant/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/umakant/cloudgateway/CloudGatewayApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
+		//Hello
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
