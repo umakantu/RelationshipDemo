2a559cddb380a51047d8ade9664489141563267c
Umakant <Umakant@DESKTOP-O49BCT7>
2021-11-16 10:45:02 +0530

    new

---
+++ jenkinsdemo/src/main/java/com/umakant/jenkinsdemo/JenkinsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.umakant.jenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+//hello
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+	}
+
+}
